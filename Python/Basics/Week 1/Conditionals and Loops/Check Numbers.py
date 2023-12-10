5f2afdf3b9364b4bf3d1f903a9579298db0ab1d4
Varnika <32481536+varnika-vjit@users.noreply.github.com>
2023-12-10 13:51:46 +0530

    Update Check Numbers.py

--- Python/Basics/Week 1/Conditionals and Loops/Check Numbers.py
+++ Python/Basics/Week 1/Conditionals and Loops/Check Numbers.py
@@ -1,1 +1,10 @@
+# Read input as sepcified in the question
+# Print output as specified in the question
+n=int(input())
 
+if n>0:
+    print("Positive")
+elif n==0:
+    print("Zero")
+else:
+    print("Negative")
